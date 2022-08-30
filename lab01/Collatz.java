4a037962d667fa003fb0b136dd8cc33532bde9c5
Audrey <audrey.burnett@berkeley.edu>
2022-08-30 01:06:43 -0700

    finished Lab01

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -5,8 +5,12 @@ public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        // TODO: Fill in this method.
-        return 1;
+        /** returns the next number in the Collatz sequence. if n is even, returns n/2. if n is odd, the next number is 3n + 1.*/
+        if (n % 2 == 0){
+            return n/2;
+        } else{
+            return (3*n) + 1;
+        }
     }
 
     public static void main(String[] args) {
