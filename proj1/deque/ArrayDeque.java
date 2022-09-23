e26552215feb2b76dcc12aaee493e9705169f83d
Audrey <audrey.burnett@berkeley.edu>
2022-09-22 18:41:41 -0700

    working on resize

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -15,14 +15,17 @@ public class ArrayDeque<T> {
 
     public void resize(int capacity){
         T[] a = (T[]) new Object[capacity];
+        T[] b = (T[]) new Object[size];
+        System.arraycopy(items, nextLast, b, 0, size-nextLast);
+        System.arraycopy(items, nextFirst, b, size-nextLast, size- (size-nextLast));
         System.arraycopy(items, 0, a, 0, size);
         items = a;
-        nextFirst = 0;
+        nextFirst = items.length - 1;
         nextLast = size;
     }
     public void addFirst(T item) {
         if (size == items.length) {
-            resize(2 * items.length);
+            resize(size*4);
         }
         items[nextFirst] = item;
         if (nextFirst == 0) {
@@ -32,22 +35,6 @@ public class ArrayDeque<T> {
         }
         size += 1;
     }
-//        if(size == 0){
-//            items[nextFirst] = item;
-//            nextFirst
-//        }
-//        if (items.length == size){
-//            resize(size*4);
-//            nextLast = size;
-//            nextFirst = size;
-//        }
-//        items[nextFirst] = item;
-//        if (nextFirst - 1 < 0){
-//            nextFirst = items.length;
-//        } else{
-//            nextFirst -= 1;
-//        }
-//        size += 1;
 
     public void addLast(T item){
         if (items.length == size){
@@ -76,7 +63,7 @@ public class ArrayDeque<T> {
 
     public void printDeque(){
         for(int i = 0; i < size; i++){
-            System.out.print(items[i] + " ");
+            System.out.print(get(i) + " ");
         }
         System.out.println(" ");
     }
@@ -85,7 +72,9 @@ public class ArrayDeque<T> {
         if(size == 0){
             return null;
         }
-        //resize
+        if(items.length >= 16 && (items.length/(size - 1)) < items.length/4){
+            resize(size*4);
+        }
         int first;
         if(nextFirst == items.length - 1){
             first = 0;
@@ -133,5 +122,17 @@ public class ArrayDeque<T> {
         return items[newIndex];
     }
 
-
+    public boolean equals(Object o){
+        if (!(o instanceof ArrayDeque)){
+            return false;
+        } else{
+            ArrayDeque <T> cast = (ArrayDeque<T>) o;
+            for (int i = 0; i < size; i++){
+                if (!(get(i).equals((ArrayDeque) cast.get(i)))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
