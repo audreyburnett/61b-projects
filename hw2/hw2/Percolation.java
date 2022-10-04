bdf7f64d1b093c0d54fe1e82d6258041aa286812
Audrey <audrey.burnett@berkeley.edu>
2022-10-03 19:40:54 -0700

    perc

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,64 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    int[][] perc;
+    int len;
+    int numberOfOpenSites;
+    WeightedQuickUnionUF group;
 
+
+    public Percolation(int N){
+        if (N <= 0){
+            throw new java.lang.IllegalArgumentException("Cannot have negative area!");
+        }
+        len = N;
+        numberOfOpenSites = 0;
+        perc = new int[N][N];
+    }
+    private int modulo(int row, int col) {
+        return (((row - 1) * len) + col);
+    }
+    public void open(int row, int col){
+        if (row >= len || col >= len){
+            throw new java.lang.IndexOutOfBoundsException("Index out of bounds!");
+        }
+        perc[row][col] = 1;
+        if ((row + 1) < len && isOpen(row + 1, col)) {
+            group.union(modulo(row, col), modulo(row + 1, col));
+        }
+        if ((row - 1) >= 0 && isOpen(row - 1, col)) {
+            group.union(modulo(row, col), modulo(row - 1, col));
+        }
+        if ((col + 1) < len && isOpen(row + 1, col)) {
+            group.union(modulo(row, col), modulo(row, col + 1));
+        }
+        if ((col - 1) >= 0 && isOpen(row + 1, col)) {
+            group.union(modulo(row, col), modulo(row, col - 1));
+        }
+        if (row == 0){
+            group.union(modulo(row, col), modulo(-1, -1));
+        }
+        if (row == len - 1){
+            group.union(modulo(row, col), modulo(-2, -2));
+        }
+        numberOfOpenSites += 1;
+    }
+    public boolean isOpen(int row, int col){
+        if (row >= len || col >= len){
+            throw new java.lang.IndexOutOfBoundsException("Index out of bounds!");
+        }
+        return perc[row][col] == 1;
+    }
+    public boolean isFull(int row, int col){
+        if (row >= len || col >= len){
+            throw new java.lang.IndexOutOfBoundsException("Index out of bounds!");
+        }
+        return group.connected(modulo(row, col), modulo(-1, -1));
+    }
+    public int numberOfOpenSites(){
+        return numberOfOpenSites;
+    }
+    public boolean percolates(){
+        return group.connected(modulo(-1, -1), modulo(-2, -2));
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,25 @@
-package hw2;
-
-public class PercolationStats {
-
-}
+//package hw2;
+//
+//public class PercolationStats {
+//    public PercolationStats(int N, int T, PercolationFactory pf){
+//        if (T <= 0){
+//            throw new java.lang.IllegalArgumentException("Can't perform negative operations!");
+//        }
+//        if (N <= 0){
+//            throw new java.lang.IllegalArgumentException("Can't have negative area!");
+//        }
+//        return;
+//    }
+//    public double mean(){
+//        return;
+//    }
+//    public double stddev(){
+//        return;
+//    }
+//    public double confidenceLow(){
+//        return;
+//    }
+//    public double confidenceHigh(){
+//        return;
+//    }
+//}
