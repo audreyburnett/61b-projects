ae2d1998723193f14ad4e4f63ff081cbd9681ace
Audrey <audrey.burnett@berkeley.edu>
2022-10-03 21:09:44 -0700

    akefaekj

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,25 +1,41 @@
-//package hw2;
-//
-//public class PercolationStats {
-//    public PercolationStats(int N, int T, PercolationFactory pf){
-//        if (T <= 0){
-//            throw new java.lang.IllegalArgumentException("Can't perform negative operations!");
-//        }
-//        if (N <= 0){
-//            throw new java.lang.IllegalArgumentException("Can't have negative area!");
-//        }
-//        return;
-//    }
-//    public double mean(){
-//        return;
-//    }
-//    public double stddev(){
-//        return;
-//    }
-//    public double confidenceLow(){
-//        return;
-//    }
-//    public double confidenceHigh(){
-//        return;
-//    }
-//}
+package hw2;
+
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+    StdStats stats;
+    StdRandom random;
+    double[] lst;
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if (T <= 0){
+            throw new java.lang.IllegalArgumentException("Can't perform negative operations!");
+        }
+        if (N <= 0){
+            throw new java.lang.IllegalArgumentException("Can't have negative area!");
+        }
+        lst = new double[T];
+
+        for (int i = 0; i < T; i ++) {
+            Percolation grid = pf.make(N);
+            while (grid.percolates() != true) {
+                int row = random.uniform(N);
+                int col = random.uniform(N);
+                grid.open(row, col);
+            }
+            lst[i] = ((double) grid.numberOfOpenSites() / (double) (N * N));
+        }
+    }
+    public double mean(){
+        return stats.mean(lst);
+    }
+    public double stddev(){
+        return stats.stddev(lst);
+    }
+    public double confidenceLow(){
+        return;
+    }
+    public double confidenceHigh(){
+        return;
+    }
+}
