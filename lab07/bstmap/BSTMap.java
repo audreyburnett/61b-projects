2585afdb4029c7660e7463171d274986d86beddc
Audrey <audrey.burnett@berkeley.edu>
2022-10-06 19:11:18 -0700

    lab 7

---
+++ lab07/bstmap/BSTMap.java
@@ -0,0 +1,37 @@
+package bstmap;
+
+import java.util.Set;
+
+public class BSTMap implements Map61B<K,V>{
+
+    public void clear(){
+
+    }
+    public boolean containsKey(K key){
+
+    }
+    public V get(K key){
+
+    }
+    public int size(){
+
+    }
+    public void put(K key, V value){
+
+    }
+    public void printInOrder(){
+
+    }
+    public Set<K> keySet(){
+        throw new UnsupportedOperationException();
+    }
+    public V remove(K key){
+        throw new UnsupportedOperationException();
+    }
+    public V remove(K key, V value){
+        throw new UnsupportedOperationException();
+    }
+    public Iterator<K> iterator(){
+        throw new UnsupportedOperationException();
+    }
+}
