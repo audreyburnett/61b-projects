20e4e609785e53fc02ea6bca31fdf37355d6e7bd
Audrey <audrey.burnett@berkeley.edu>
2022-10-06 19:18:37 -0700

    BOO

--- lab07/bstmap/BSTMap.java
+++ lab07/bstmap/BSTMap.java
@@ -3,9 +3,24 @@ package bstmap;
 import java.util.Set;
 
 public class BSTMap implements Map61B<K,V>{
+    private class Node{
+        private K key;
+        private V val;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V val, int size){
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
 
     public void clear(){
-
+        for (int i = 0; i < size; i++){
+            
+        }
     }
     public boolean containsKey(K key){
 
